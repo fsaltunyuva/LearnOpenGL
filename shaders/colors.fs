afbf2f72f8a1c380b9dbdc228a6bb16f3d333e40
Furkan Safa Altunyuva <103141929+fsaltunyuva@users.noreply.github.com>
2026-08-17 17:49:26 +0300

    basic lighting chapter until one last thing

--- shaders/colors.fs
+++ shaders/colors.fs
@@ -1,10 +1,26 @@
 #version 330 core
-out vec4 FragColor;
 
 uniform vec3 objectColor;
 uniform vec3 lightColor;
+uniform vec3 lightPos;
+
+in vec3 Normal;
+in vec3 FragPos; // will be interpolated in GPU from the 3 world position vectors of the fragment triangle
+
+out vec4 FragColor;
 
 void main()
 {
-   FragColor = vec4(lightColor * objectColor, 1.0);
+   vec3 norm = normalize(Normal);
+   vec3 lightDir = normalize(lightPos - FragPos);
+
+   // diffuse
+   float diff = max(dot(norm, lightDir), 0.0);
+   vec3 diffuse = diff * lightColor;
+
+   float ambientStrength = 0.1;
+   vec3 ambient = ambientStrength * lightColor;
+
+   vec3 result = (ambient + diffuse) * objectColor;
+   FragColor = vec4(result, 1.0);
 }
