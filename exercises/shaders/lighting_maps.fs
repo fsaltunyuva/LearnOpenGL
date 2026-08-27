4567e4b6bfb558dd37adca5d6b3cbfd4e0b358d2
Furkan Safa Altunyuva <103141929+fsaltunyuva@users.noreply.github.com>
2026-08-27 14:34:30 +0300

    lighting maps exercises

---
+++ exercises/shaders/lighting_maps.fs
@@ -0,0 +1,49 @@
+#version 330 core
+
+struct Material {
+   sampler2D diffuse;
+   sampler2D specular; // specular highlight
+   sampler2D emission;
+   float shininess; // scattering of the specular highlight
+};
+
+struct Light {
+   vec3 position;
+
+   vec3 ambient;
+   vec3 diffuse;
+   vec3 specular;
+};
+
+uniform Material material;
+uniform Light light;
+uniform vec3 viewPos;
+
+in vec2 TexCoords;
+in vec3 Normal;
+in vec3 FragPos; // will be interpolated in GPU from the 3 world position vectors of the fragment triangle
+
+out vec4 FragColor;
+
+void main()
+{
+   // ambient
+   vec3 ambient  = light.ambient * vec3(texture(material.diffuse, TexCoords));
+
+   // diffuse
+   vec3 norm = normalize(Normal);
+   vec3 lightDir = normalize(light.position - FragPos);
+   float diff = max(dot(norm, lightDir), 0.0);
+   vec3 diffuse  = light.diffuse * diff * vec3(texture(material.diffuse, TexCoords));
+
+   // specular
+   vec3 viewDir = normalize(viewPos - FragPos);
+   vec3 reflectDir = reflect(-lightDir, norm); // lightDir points from the fragment towards the light source, so we negate it
+   float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+   vec3 specular = light.specular * spec * (/*vec3(1.0,1.0,1.0) - */vec3(texture(material.specular, TexCoords))); // EXERCISE 2
+
+   vec3 emission = texture(material.emission, TexCoords).rgb; // EXERCISE 4
+
+   vec3 result = ambient + diffuse + specular + emission;
+   FragColor = vec4(result, 1.0);
+}
